c493afb41914a77eb7fd76fb96f8e741b9e281b4
BikashKakati <bikashkakati796@gmail.com>
2023-06-24 12:13:24 +0530

    Transfer Redux Provider
    Transfer redux provider to main from app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import { Provider } from 'react-redux';
-import store from './store/Store';
 import Navbar from './components/navbar/Navbar';
 import Home from './pages/home/Home';
 import Cart from './pages/cart/Cart';
@@ -11,16 +9,13 @@ function App() {
 
   return (
     <>
-      <Provider store={store}>
         <Router>
           <Navbar/>
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/cart" element={<Cart />}></Route>
           </Routes>
-
         </Router>
-      </Provider>
 
     </>
   )
