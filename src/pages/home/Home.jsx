56d03945be138b531e4312f54acad55a6a0e06a5
BikashKakati <bikashkakati796@gmail.com>
2023-07-07 23:51:57 +0530

    Adding sort feature
    Now we have sort features
    -A to Z and Z to A feature
    -Price decreasing and increasing

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import Cards from '../../components/cards/Cards'
 const Home = () => {
   const [cards, setCards] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [sortCallback, setSortCallback] = useState(()=>()=>{});
   const url = "https://fakestoreapi.com/products";
 
   useEffect(() => {
@@ -33,13 +34,35 @@ const Home = () => {
   return (
     <>
       <div className="container">
-        <div className="filter-box"></div>
+        <div className="filter-box">
+          <h3 className="filter-heading">Product Filter</h3>
+
+
+          <ul className="filter-list">
+            <li className="filters" onClick={()=>setSortCallback(()=>(a,b)=>a.price-b.price)}>
+              <input type="radio" id="ascending" name="filter" value="ascending"/>
+              <label htmlFor="ascending">Ascending</label>
+            </li>
+            <li className="filters" onClick={()=>setSortCallback(()=>(a,b)=>b.price-a.price)}>
+              <input type="radio" id="descending" name="filter" value="descending" />
+              <label htmlFor="descending">Descending</label></li>
+            <li className="filters" onClick={()=>setSortCallback(()=>(a,b)=>a.title.localeCompare(b.title))}>
+              <input type="radio" id="atoz" name="filter" value="atoz" />
+              <label htmlFor="atoz">A to Z</label>
+            </li>
+            <li className="filters" onClick={()=>setSortCallback(()=>(a,b)=>b.title.localeCompare(a.title))}>
+              <input type="radio" id="ztoa" name="filter" value="ztoa" />
+              <label htmlFor="ztoa">Z to A</label>
+            </li>
+          </ul>
+          
+        </div>
         <div className="products-box">
           {
             loading ?
               <div className='loading'></div>
               :
-              (cards.map(card => (<Cards key={card.id} card={card} />)))
+              (cards.sort(sortCallback).map(card => (<Cards key={card.id} card={card} />)))
           }
         </div>
       </div>
